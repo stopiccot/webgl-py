492a900754d8c47864f50299194675c3581502c9
stopiccot <alexey.petruchik@gmail.com>
2012-12-17 02:34:51 +0300

    rollback

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,4 @@
-import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'flask_env', 'bin', 'python')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
-
 from app import app as application
-#app.app.run()
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
