ee6194b27a22a775603f48b80e8fb7f0c79be656
stopiccot <alexey.petruchik@gmail.com>
2013-11-24 17:35:34 +0300

    updated jquery to 2.0.3

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def js(file):
     return coffeescript.compile_file('views/' + file + '.coffee')
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug = True)
+    app.run(host = '0.0.0.0', port = 4568, debug = True)
