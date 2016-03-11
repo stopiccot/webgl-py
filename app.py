2e725078d5cba6ae82642c493843a771f1793385
stopiccot <alexey.petruchik@gmail.com>
2016-03-12 00:35:41 +0300

    subdomain handling

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import coffeescript, flask
 app = flask.Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return flask.render_template('index.html')
+
+@app.route("/", subdomain = "qr")
+def qr():
+	return flask.render_template('qr_index.html')
 
 @app.route('/js/<file>')
 def js(file):
     return coffeescript.compile_file('views/' + file + '.coffee')
 
 if __name__ == "__main__":
+    app.config.update(SERVER_NAME = 'stopiccot.dev:4568')
     app.run(host = '0.0.0.0', port = 4568, debug = True)
