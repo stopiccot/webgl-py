972c1159d67498ab26ec8324ea083ae6cc98057c
stopiccot <alexey.petruchik@gmail.com>
2016-03-17 13:47:04 +0300

    subdomain fix

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def qr():
 def js(file):
     return coffeescript.compile_file('views/' + file + '.coffee')
 
+app.add_url_rule(app.static_url_path + '/<path:filename>',
+                 endpoint = 'static',
+                 view_func = app.send_static_file,
+                 subdomain = 'qr')
+
 if __name__ == "__main__":
     app.config.update(SERVER_NAME = 'stopiccot.dev:4568')
     app.run(host = '0.0.0.0', port = 4568, debug = True)
